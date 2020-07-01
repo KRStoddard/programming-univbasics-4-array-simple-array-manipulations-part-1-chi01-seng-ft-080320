cbf2f33dd75ee5413269588a9b61be189687466a
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-01 19:16:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,8 +5,11 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 def using_pop(array)
-  array.pop(2)
+  array.pop
 end
+def pop_with_args(array)
+  array.pop(2)
+end 
 def using_shift(array) 
   array.shift 
 end
